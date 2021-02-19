dda81784b5f7e6b4438d6b06e1c303c921d347ca
iamtimleonard <timleonardcello@gmail.com>
2021-02-19 10:47:13 -0500

    commit to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     };
     getPolls();
   }, []);
+
   const fetchPolls = async () => {
     const res = await fetch(
       process.env.port
@@ -23,24 +24,29 @@ const App = () => {
     const data = await res.json();
     return data;
   };
+
   const handleVote = (choice, pollId) => {
     let pollFromState = polls.find((poll) => poll._id === pollId);
-
     let remainingPolls = polls.filter((poll) => poll._id !== pollId);
+
     pollFromState.options.forEach((option) => {
       option.id === choice && option.votes++;
     });
+
     axios
       .post(`http://localhost:5000/polls/vote/${pollId}`, pollFromState)
       .then((res) => console.log(res.data));
+
     setPolls([pollFromState, ...remainingPolls]);
   };
+
   const handleCreate = (pollData) => {
     axios.post(`http://localhost:5000/polls/add`, pollData).then((res) => {
       pollData._id = res.data;
       setPolls([...polls, pollData]);
     });
   };
+
   return (
     <Router>
       <nav>
